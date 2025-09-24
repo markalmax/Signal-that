d65a903ccf5672d70c21d2c1f44de1fc4d897217
markalmax <markalmax782@gmail.com>
2025-09-24 05:12:29 +0300

    Movement update

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -6,17 +6,23 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("Crouch"):
+		scale.y /= 2
+		$CollisionShape2D.shape.extents.y /= 2
+		$CollisionShape2D.position.y += $CollisionShape2D.shape.extents.y
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_just_released("Crouch"):
+		scale.y *= 2
+		$CollisionShape2D.shape.extents.y *= 2
+		$CollisionShape2D.position.y -= $CollisionShape2D.shape.extents.y / 2
+	
+	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
